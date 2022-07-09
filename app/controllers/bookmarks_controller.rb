1e14dc403f6f66ec1766bd19567105724f6383f2
Mike Bennett <mvbennett9@gmail.com>
2022-07-09 17:01:05 +0900

    Did ost of it but stuck on destroy

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @movies = Movie.all
+  end
+
+  def create
+    movie = Movie.find(params[:bookmark][:movie])
+    list = List.find(params[:list_id])
+    bookmark = Bookmark.new(bookmark_params)
+    bookmark.list = list
+    bookmark.movie = movie
+    bookmark.save
+
+    redirect_to list_path(list)
+  end
+
+  def destroy
+    list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to list_path(list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
